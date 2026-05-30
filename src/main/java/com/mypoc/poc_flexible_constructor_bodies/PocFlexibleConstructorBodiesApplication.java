ef9d48a9f3dd18a4a412f9d180d09f20c5333d73
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-30 11:07:08 -0300

    feat: first commit

---
+++ src/main/java/com/mypoc/poc_flexible_constructor_bodies/PocFlexibleConstructorBodiesApplication.java
@@ -0,0 +1,13 @@
+package com.mypoc.poc_flexible_constructor_bodies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocFlexibleConstructorBodiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocFlexibleConstructorBodiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypoc/poc_flexible_constructor_bodies/PocFlexibleConstructorBodiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypoc.poc_flexible_constructor_bodies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocFlexibleConstructorBodiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
